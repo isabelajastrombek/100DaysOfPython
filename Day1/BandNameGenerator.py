8e854bf71426407a3a25057c64cc2e842565ed89
IsaPizza <68596490+isabelajastrombek@users.noreply.github.com>
2026-05-29 14:42:21 -0300

    Day 1

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Hello, " + input("What's your name?\n") + "!")
+print("Welcome to band name generator!\n")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print ("Your band name should be " + city, pet + "!")
